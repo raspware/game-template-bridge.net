de051620c4e72c15ecefb1ca296c8fd9079dfdd7
Raspware <Raspware@localhost>
2018-04-25 17:01:33 +0100

    Removed autio and image loading from the 'data' class.

--- ExampleGame/Data.cs
+++ ExampleGame/Data.cs
@@ -1,16 +1,10 @@
-﻿using System.Threading.Tasks;
-using Bridge.Html5;
-
-namespace Raspware.ExampleGame
+﻿namespace Raspware.ExampleGame
 {
 	public sealed class Data
 	{
 		public int Score;
 		public int Lives;
 		public int TimePassed;
-		public HTMLImageElement Image { get; set; }
-		public HTMLAudioElement OpeningMusic { get; set; }
-
 		public static Data Instance { get; } = new Data();
 
 		private Data()
@@ -22,35 +16,6 @@ namespace Raspware.ExampleGame
 			Score = 0;
 			Lives = 3;
 			TimePassed = 0;
-		}
-
-		public Task<HTMLImageElement> LoadImage()
-		{
-			var promise = new TaskCompletionSource<HTMLImageElement>();
-			var image = new HTMLImageElement();
-
-			image.Src = "/images/test.png";
-			image.AddEventListener(EventType.LoadedData, _ => promise.SetResult(image));
-
-			Image = image; // will get raised, when the work is done
-
-			return promise.Task;
-		}
-
-		public Task<HTMLAudioElement> LoadAudio()
-		{
-			var promise = new TaskCompletionSource<HTMLAudioElement>();
-			var audio = new HTMLAudioElement()
-			{
-				Src = "audio/Theme.ogg",
-				OnLoadedData = (ev) =>
-				{
-					promise.SetResult(ev.CurrentTarget);
-				}
-			};
-
-			OpeningMusic = audio; // will get raised, when the work is done
-			return promise.Task;
 		}
 	}
 }
