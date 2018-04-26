320a9e54650a3ec33410cd7c0f808ea246e4729b
Raspware <shaun.maddocks@gmail.com>
2018-04-26 22:32:58 +0100

    Added 'TemporaryTouch' class using the 'Button' as the base class.

---
+++ GameEngine/Input/Touch/Buttons/TemporaryTouch.cs
@@ -0,0 +1,13 @@
+﻿namespace Raspware.GameEngine.Input.Touch.Buttons
+{
+	public sealed class TemporaryTouch : Button
+	{
+		public TemporaryTouch(int x, int y, double radius) : base(x, y, radius) { }
+
+		public void Reset(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+}
