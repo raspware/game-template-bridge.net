ccf91aedad3cc8c64f8e2b8adae79990e3faabfe
Raspware <Raspware@localhost>
2018-04-30 17:39:06 +0100

    Fixed intro fade.

--- ExampleGame/Stage/Opening.cs
+++ ExampleGame/Stage/Opening.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Bridge.Html5;
+﻿using Bridge.Html5;
 using Raspware.GameEngine;
 using Raspware.GameEngine.Rendering;
 
@@ -19,7 +18,11 @@ namespace Raspware.ExampleGame.Stage
 		{
 			_music = new HTMLAudioElement() { Src = Resource.Audio.Theme };
 			_music.Play();
-			_music.AddEventListener(EventType.Ended, ev => { _musicPlayed = true; });
+			_music.AddEventListener(EventType.Ended, ev =>
+			{
+				_musicPlayed = true;
+				_timePassed = 0;
+			});
 		}
 
 		public void Draw()
@@ -30,7 +33,6 @@ namespace Raspware.ExampleGame.Stage
 			var levelContext = Layers.Instance.GetLayer(Layers.Id.Level).GetContext();
 			var resolution = Resolution.Instance;
 
-			levelContext.ClearRect(0, 0, resolution.Width, resolution.Height); // Clear
 			levelContext.FillStyle = "rgb(0,0,0)";
 			levelContext.FillRect(0, 0, resolution.Width, resolution.Height); // Clear
 
@@ -42,13 +44,9 @@ namespace Raspware.ExampleGame.Stage
 			levelContext.Font = resolution.RenderAmount(6).ToString() + "px Consolas, monospace";
 			levelContext.FillText(_message, resolution.RenderAmount(4), resolution.RenderAmount(96));
 
-
 			levelContext.BeginPath();
 			levelContext.FillStyle = $"rgba(0,0,0,{_alpha})";
 
-			Console.WriteLine(_alpha);
-
-			
 			levelContext.FillRect(0, 0, resolution.Width, resolution.Height); // Clear
 			levelContext.ClosePath();
 		}
@@ -58,13 +56,23 @@ namespace Raspware.ExampleGame.Stage
 			_timePassed += ms;
 			_message = _timePassed.ToString();
 
-			if (_timePassed > 2000)
-				_alpha = (1 - (double)((double)_timePassed - 2000 / (double)4000)).ToString();
+			if (!_musicPlayed)
+			{
+				if (_timePassed < 1000)
+					return Id;
 
-			if (_timePassed > 6000)
-				_alpha = "0";
+				// fade in
+				if (_timePassed < 4000)
+					_alpha = (1 - _timePassed / (double)4000).ToString();
+				else
+					_alpha = "0";
+				return Id;
+			}
 
-			if (_musicPlayed)
+			// fade out
+			if (_timePassed < 1000)
+				_alpha = (_timePassed / (double)1000).ToString();
+			else
 				return Stage.Id.Title;
 
 			return Id;
