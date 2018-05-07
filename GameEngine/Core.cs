61dc589d7c417cc8ec288bea392c38798c7eb378
Raspware <shaun.maddocks@gmail.com>
2018-05-07 22:34:43 +0100

    Created 'Core' with some interfaces.

---
+++ GameEngine/Core.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Bridge.Html5;
+using Raspware.GameEngine.Input;
+using Raspware.GameEngine.Input.Shared;
+using Raspware.GameEngine.Rendering;
+
+namespace Raspware.GameEngine
+{
+	public static partial class Game
+	{
+		private sealed class Core : ICore, ICoreResolution, ICoreButtons, ICoreStageFactory, ICoreRun
+		{
+			private IStage _stage { get; set; }
+			private int _lastFrame { get; set; }
+			private Layers _layers { get; set; }
+			private Func<int, IStage> _getStage { get; }
+			public IActions ActionRaiser { get; }
+			public IButtons Buttons { get; }
+			public Layer Controls { get; private set; }
+			private Resolution _resolution { get; set; }
+			public ICoreStageFactory Resolution(Resolution resolution)
+			{
+				if (resolution == null)
+					throw new ArgumentNullException(nameof(resolution));
+
+				_resolution = resolution;
+
+				// TODO: A
+				return this;
+			}
+
+			private void InitaliseLayers()
+			{
+				if (_resolution == null)
+					throw new ArgumentNullException(nameof(_resolution));
+
+				if (_layers != null)
+				{
+					Console.WriteLine("Layers already initalised!");
+					return;
+				}
+
+				_layers = new Layers(_resolution);
+			}
+
+			public void Run(int startStageId)
+			{
+				_stage = _getStage(startStageId);
+				Tick();
+			}
+
+			public ICoreRun StageFactory(Func<ICore, int, IStage> stageFactory)
+			{
+				return this;
+			}
+
+			public Resolution GetResolution()
+			{
+				return _resolution;
+			}
+
+			private void Tick()
+			{
+				var now = (int)Window.Performance.Now();
+				var ms = now - _lastFrame;
+
+				_layers.Resize();
+				var returnedId = _stage.Update(ms);
+				if (_stage.Id == returnedId)
+					_stage.Draw();
+				else
+					_stage = _getStage(returnedId);
+
+				_lastFrame = now;
+				Window.RequestAnimationFrame(Tick);
+			}
+		}
+	}
+}
---
+++ GameEngine/ICoreInterfaces.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ProductiveRage.Immutable;
+using Raspware.GameEngine.Input.Shared;
+using Raspware.GameEngine.Rendering;
+
+namespace Raspware.GameEngine
+{
+	public interface ICoreResolution
+	{
+		ICoreButtons Resolution(Resolution resolution);
+	}
+
+	public interface ICoreButtons
+	{
+		ICoreStageFactory Buttons(NonNullList<Button> Buttons);
+	}
+
+	public interface ICoreStageFactory
+	{
+		ICoreRun StageFactory(Func<ICore, int, IStage> stageFactory);
+	}
+
+	public interface ICoreRun
+	{
+		void Run(int startStageId);
+	}
+
+	public interface ICore
+	{
+		Resolution GetResolution();
+	}
+}
