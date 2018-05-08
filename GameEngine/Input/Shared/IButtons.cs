11ec8e1bedf4193fc2d73b92f1bcbbdf39633780
Raspware <Raspware@localhost>
2018-05-08 15:17:17 +0100

    (Broken) Added a 'GetButton' helper.

--- GameEngine/Input/Shared/IButtons.cs
+++ GameEngine/Input/Shared/IButtons.cs
@@ -1,9 +1,18 @@
-﻿using ProductiveRage.Immutable;
+﻿using System.Linq;
+using ProductiveRage.Immutable;
 
 namespace Raspware.GameEngine.Input.Shared
 {
 	public interface IButtons
 	{
 		NonNullList<Button> Buttons { get; }
+	}
+
+	public static class ButtonHelper
+	{
+		public static Button GetButton(this NonNullList<Button> buttons, int buttonId)
+		{
+			return buttons.Where(button => button.Id == buttonId).FirstOrDefault();
+		}
 	}
 }
