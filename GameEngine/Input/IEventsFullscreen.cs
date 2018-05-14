4cd34432578c3d595661761d835566713a13fc7f
Raspware <Raspware@localhost>
2018-05-14 10:47:12 +0100

    Some TODO's to implement next.

--- GameEngine/Input/IEventsFullscreen.cs
+++ GameEngine/Input/IEventsFullscreen.cs
@@ -3,5 +3,10 @@
 	public interface IEventsFullscreen
 	{
 		void ApplyFullscreenOnPressUp();
+
+		// TODO: Add a method for detecting if the 'wrapper' is already set as fullscreen or not.
+		// This will help with not applying the fullscreen code multiple times and also aid a
+		// fullscreen toggle.
+		// TODO: Create an 'exit-out-of-fullscreen' method.
 	}
 }
