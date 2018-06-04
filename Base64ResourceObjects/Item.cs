98dcf8aa12634c29da078d8670735a143fc25b4d
Raspware <Raspware@localhost>
2018-06-04 11:57:52 +0100

    Added 'Base64ResourceObjects' project.

---
+++ Base64ResourceObjects/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raspware.GameEngine.Base64ResourceObjects
+{
+	public sealed class Item
+	{
+		public Item(string type, Dictionary<string, string> dictionary)
+		{
+			if (string.IsNullOrWhiteSpace(type))
+				throw new ArgumentException(nameof(type));
+			if (dictionary == null)
+				throw new ArgumentNullException(nameof(dictionary));
+
+			Type = type;
+			Dictionary = dictionary;
+		}
+		
+		public string Type { get; }
+		public Dictionary<string, string> Dictionary { get; }
+	}
+}
