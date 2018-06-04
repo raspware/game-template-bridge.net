572a29710220552256ced464f83cd2ed8b81e409
Raspware <Raspware@localhost>
2018-06-04 12:29:08 +0100

    Updated and moved all relating 'resource' files into the 'Example Game' project since as we need to get the namespace.

---
+++ ExampleGame/Resources/Resources.cs
@@ -0,0 +1,11 @@
+namespace ExampleGame.Resources 
+{
+	public static class Audio
+	{
+		public const string Theme = "Theme"; 
+	}
+	public static class Images
+	{
+		public const string Test = "Test"; 
+	}
+}
