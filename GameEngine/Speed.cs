4f8fe5f8e6aae99cf9f0ef790f9681c4434ad7d9
Raspware <shaun.maddocks@gmail.com>
2018-06-07 22:30:20 +0100

    Implemented Speed.

---
+++ GameEngine/Speed.cs
@@ -0,0 +1,37 @@
+﻿namespace Raspware.GameEngine
+{
+	public class Speed
+	{
+		public readonly double Amount;
+		public double Current { private set; get; }
+		public readonly double Max;
+		public readonly double Min;
+
+		public Speed(double amount, double max, double min = 0)
+		{
+			Amount = amount;
+			Max = max;
+			Min = min;
+			Current = 0;
+		}
+
+		public void Update(int ms, bool increase = false)
+		{
+			Update((double)ms, increase);
+		}
+
+		public void Update(double ms, bool increase = false)
+		{
+			var frameAmount = ms * Amount;
+			if (increase)
+				Current += frameAmount;
+			else
+				Current -= frameAmount;
+
+			if (Current < Min)
+				Current = Min;
+			else if (Current > Max)
+				Current = Max;
+		}
+	}
+}
