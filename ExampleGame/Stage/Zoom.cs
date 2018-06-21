3adc6307df70041a967bd83fdad1d621580b4d92
Raspware <Raspware@localhost>
2018-06-21 14:34:13 +0100

    Removed old experimental work.

--- ExampleGame/Stage/Zoom.cs
+++ ExampleGame/Stage/Zoom.cs
@@ -4,7 +4,6 @@ using ProductiveRage.Immutable;
 using Raspware.ExampleGame.Resources;
 using Raspware.GameEngine;
 using Raspware.GameEngine.Input;
-using Raspware.GameEngine.Rendering;
 
 namespace Raspware.ExampleGame.Stage
 {
@@ -14,7 +13,6 @@ namespace Raspware.ExampleGame.Stage
 		private bool _renderedControls;
 		private bool _first;
 		private HTMLImageElement _image;
-		private Camera _camera;
 		private ResourcePool _resourcePool;
 		private ICore _core { get; }
 
@@ -29,12 +27,6 @@ namespace Raspware.ExampleGame.Stage
 			_core.Layers.Reset(NonNullList.Of(0));
 			_core.ActivateActions();
 			_resourcePool = new ResourcePool();
-			_camera = new Camera(
-				new NumberWithConstraints(0.02, 0.00001),
-				4,
-				new NumberWithConstraints(5, 0.01),
-				new NumberWithConstraints(5, 0.01)
-			);
 		}
 
 		public void Draw()
@@ -59,19 +51,6 @@ namespace Raspware.ExampleGame.Stage
 			_image = _resourcePool.Images[Image.Background];
 
 			levelContext.ClearRect(0, 0, resolution.Width, resolution.Height);
-			levelContext.DrawImage(
-				_image,
-				(int)(_camera.X * _camera.ZoomAmount),
-				(int)(_camera.Y * _camera.ZoomAmount),
-				(int)(_image.Width * _camera.ZoomAmount),
-				(int)(_image.Height * _camera.ZoomAmount)
-			);
-
-			levelContext.FillStyle = "white";
-			levelContext.Font = resolution.RenderAmount(10).ToString() + "px Consolas, monospace";
-			levelContext.FillText("zoom: " + _camera.Zoom.Current.ToString(), resolution.RenderAmount(4), resolution.RenderAmount(12));
-			levelContext.FillText("x: " + _camera.MoveX.Current.ToString(), resolution.RenderAmount(4), resolution.RenderAmount(18));
-			levelContext.FillText("y: " + _camera.MoveY.Current.ToString(), resolution.RenderAmount(4), resolution.RenderAmount(24));
 
 			if (!_renderedControls)
 			{
@@ -99,54 +78,6 @@ namespace Raspware.ExampleGame.Stage
 			var right = _core.ActionEvents[DefaultActions.Right];
 			var button1 = _core.ActionEvents[DefaultActions.Button1];
 			var menu = _core.ActionEvents[DefaultActions.Menu];
-
-			if (up.PressedDown() || down.PressedDown())
-				_camera.MoveY.Update(ms, true);
-			else
-				_camera.MoveY.Update(ms, false, true);
-
-			if (left.PressedDown() || right.PressedDown())
-				_camera.MoveX.Update(ms, true);
-			else
-				_camera.MoveX.Update(ms, false, true);
-
-			if (button1.PressedDown() || menu.PressedDown())
-				_camera.Zoom.Update(ms, true);
-			else
-				_camera.Zoom.Update(ms, false, true);
-
-
-			if (down.PressedDown())
-			{
-				_camera.UpdateYDirection(true);
-			}
-
-			if (up.PressedDown())
-			{
-				_camera.UpdateYDirection();
-			}
-
-			if (left.PressedDown())
-			{
-				_camera.UpdateXDirection(true);
-			}
-
-			if (right.PressedDown())
-			{
-				_camera.UpdateXDirection();
-			}
-
-			if (button1.PressedDown())
-			{
-				_camera.UpdateZoom(true);
-			}
-
-			if (menu.PressedDown())
-			{
-				_camera.UpdateZoom();
-			}
-
-			_camera.Update();
 
 			data.TimePassed += ms;
 			_message = data.TimePassed.ToString();
